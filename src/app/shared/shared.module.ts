dc970e03b72f7da3f4b7bd10c4a5abccdfe95020
lvm3632 <lvm3632@gmail.com>
2022-03-25 17:54:28 -0600

    Home changes
    Changes in the home

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,13 @@ import { StyleClassModule } from 'primeng/styleclass';
 import { RippleModule } from 'primeng/ripple';
 import { FooterComponent } from './layout/footer/footer.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [FooterComponent, SidebarComponent],
-  imports: [CommonModule, RippleModule, StyleClassModule],
+  imports: [CommonModule, RippleModule, StyleClassModule, RouterModule],
   exports: [FooterComponent, SidebarComponent],
 })
 export class SharedModule {}
